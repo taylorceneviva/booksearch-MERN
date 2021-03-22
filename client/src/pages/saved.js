1b1f13ca208593cc5e3e180c32a9ac1c47ad4390
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-22 16:56:12 -0400

    add search and saved page files

---
+++ client/src/pages/saved.js
@@ -0,0 +1,2 @@
+// Renders all books saved to the Mongo database. User has an option to "View" the book, 
+// bringing them to the book on Google Books, or "Delete" a book, removing it from the Mongo database.
---
+++ client/src/pages/search.js
@@ -0,0 +1,3 @@
+// User can search for books via the Google Books API and render them here. 
+// User has the option to "View" a book, bringing them to the book on Google Books, or "Save" 
+// a book, saving it to the Mongo database.
