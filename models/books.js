4a003747fc59a016f0fd60bbaf8a3c890ed0e17b
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-22 22:04:56 -0400

    set up books schema

--- models/books.js
+++ models/books.js
@@ -1,6 +1,17 @@
 // Model for books API 
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const booksSchema = new Schema({
 // - Title
 // -authors 
 // - description 
 // - image 
 // - link
+
+});
+
+const Books = mongoose.model("Books", booksSchema);
+
+module.exports = Books;
