2087e96437a2091b780b54f439173ce4ce297496
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-22 21:38:08 -0400

    add routes folder/file/pseudo code

---
+++ routes/api-routes.js
@@ -0,0 +1,7 @@
+// API Routes
+
+// * `/api/books` (get) - Should return all saved books as JSON.
+
+// * `/api/books` (post) - Will be used to save a new book to the database.
+
+// * `/api/books/:id` (delete) - Will be used to delete a book from the database by Mongo `_id`.
